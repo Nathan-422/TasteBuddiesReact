a07382121696834c552db9d707841d016199953c
Nathan Wright <natiouswright@gmail.com>
2023-10-25 15:45:37 -0500

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      fragments: 'syntax',
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      fragment: 'syntax',
+      version: 'detect',
+    },
+  },
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# TasteBuddiesClient
+
+This is a react frontend of the Taste Buddies project built by Team Kat during Lauchcode's Sep2022 lc_101 course.
+
+## Usage
+
+The project is built using Vite. To run the dev server, navigate to the root of the project and run
+
+```
+npm run dev
+```
