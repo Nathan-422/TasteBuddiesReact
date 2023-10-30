d5c4db84fa3c8b7dbc7d111621a9e3de9af56ffc
Nathan Wright <natiouswright@gmail.com>
2023-10-30 12:54:45 -0500

    Updated routes and added some test links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
 	return (
 		<>
 			<h2>Home</h2>
 			<p>Home works</p>
+			<ul>
+				<li><Link to={"/"}>Home</Link></li>
+				<li><Link to={"/login"}>Login</Link></li>
+				<li><Link to={"/signout"}>Sign out</Link></li>
+				<li><Link to={"/events"}>Events</Link></li>
+			</ul>
 		</>
 	)
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
-import { routerProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { useAuth } from '../providers/authProvider'
 import { ProtectedRoute } from '../ProtectedRoute'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import SignOut from '../pages/SignOut'
 import Events from '../pages/Events'
 
 const Routes = () => {
@@ -30,5 +31,26 @@ const Routes = () => {
 				},
 			],
 		},
+		{
+			path: '/signout',
+			element: <SignOut />,
+		},
 	]
+
+	const routesForNotAuthenticatedOnly = [
+		{
+			path: '/login',
+			element: <Login />,
+		},
+	]
+
+	const router = createBrowserRouter([
+		...routesForPublic,
+		...(!token ? routesForAuthenticatedOnly : []),
+		...routesForNotAuthenticatedOnly,
+	])
+
+	return <RouterProvider router={router} />
 }
+
+export default Routes
