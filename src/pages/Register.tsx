bb24987b4e0a86d82da01f6334608bd8f118bbd3
Nathan Wright <natiouswright@gmail.com>
2023-11-02 14:46:52 -0500

    Started on registration form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,44 @@
 import { Helmet } from 'react-helmet'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../providers/authProvider'
+import Auth from '../services/AuthenticationService'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
+
+interface IFormInput {
+	email: string
+	displayName: string
+	password: string
+	confirmPassword: string
+}
+
 export default function Register() {
+	const { setToken } = useAuth()
+	const [errorRes, setErrorRes] = useState(null)
+	const navigate = useNavigate()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IFormInput>()
+
+	const onSubmit: SubmitHandler<IFormInput> = (data) => {
+		Auth.login(data)
+			.then((response) => {
+				setToken(response.data.idToken)
+				navigate('../event', { replace: true, relative: 'path' })
+				// TODO: continue form success message from here
+			})
+			.catch((error) => {
+				// console.log(error.response)
+				setErrorRes(error.response.status)
+				// TODO: add error message here
+			})
+			.finally(() => {
+				// TODO: probably remove this? Clear password
+			})
+	}
+
 	return (
 		<>
 			<Helmet>
@@ -7,6 +46,94 @@ export default function Register() {
 			</Helmet>
 			<h1>Register</h1>
 			<p>Registration page works</p>
+			<form
+				className="[&>label>input]:rounded-md [&>label>input]:border-2 [&>label>input]:border-gray-400"
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<label htmlFor="email" content="Email">
+					Email
+					<input
+						type="email"
+						id="email"
+						{...register('email', {
+							required: 'An email address is required',
+							pattern: {
+								value: /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+								message: 'Must be an email',
+							},
+						})}
+					/>
+				</label>
+				{errors.email?.type === 'required' && (
+					<p role="alert">{errors.email.message}</p>
+				)}
+				{errors.email?.type === 'pattern' && (
+					<p role="alert">{errors.email.message}</p>
+				)}
+				<label htmlFor="displayName" content="displayName">
+					Display Name
+					<input
+						type="text"
+						id="display-name"
+						{...register('displayName', {
+							required: 'A display name is required!',
+							minLength: {
+								value: 3,
+								message: 'Display name must be at least 3 characters.',
+							},
+						})}
+					/>
+				</label>
+				{errors.displayName?.type === 'required' && (
+					<p role="alert">{errors.displayName.message}</p>
+				)}
+				{errors.displayName?.type === 'minLength' && (
+					<p role="alert">{errors.displayName.message}</p>
+				)}
+				<label htmlFor="password">
+					Password
+					<input
+						type="password"
+						id="password"
+						{...register('password', {
+							required: 'A password is required',
+							minLength: {
+								value: 7,
+								message: 'Password must be at least 7 characters',
+							},
+						})}
+					/>
+				</label>
+				<label htmlFor="confirmPassword">
+					Password
+					<input
+						type="password"
+						id="confirmPassword"
+						{...register('password', {
+							required: 'A password is required',
+							minLength: {
+								value: 7,
+								message: 'Password must be at least 7 characters',
+							},
+						})}
+					/>
+				</label>
+				{errors.password?.type === 'required' && (
+					<p role="alert">{errors.password.message}</p>
+				)}
+				{/* TODO: Write password comparison   */}
+				{errors.password?.type === 'required' && (
+					<p role="alert">{errors.password.message}</p>
+				)}
+				<button
+					className="rounded-md bg-yellow-400 px-3 py-1 shadow-sm"
+					type="submit"
+					id="submit"
+				>
+					Submit
+				</button>
+				{errorRes && <p role="alert">{errorRes}</p>}
+			</form>
 		</>
 	)
 }
