38b2042b3e76bbcf05194e4e7815400256530bf1
Nathan Wright <natiouswright@gmail.com>
2023-11-06 15:53:28 -0600

    Added server failure to registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -32,16 +32,26 @@ export default function Register() {
 	const watchPassword = watch('password')
 	const watchConfirmPassword = watch('confirmPassword')
 
+	const [submitted, setSubmitted] = useState(false)
+	const [submissionError, setSubmissionError] = useState('')
 	const onSubmit: SubmitHandler<IFormInput> = (data) => {
+		setSubmitted(() => {
+			return true
+		})
+
 		Auth.login(data)
 			.then((response) => {
 				setToken(response.data.idToken)
 				navigate('../event', { replace: true, relative: 'path' })
-				// TODO: continue form success message from here
 			})
 			.catch((error) => {
 				// console.log(error.response)
-				setErrorRes(error.response.status)
+				setErrorRes(() => {
+					return error.response.status
+				})
+				setSubmissionError(() => {
+					return 'Registration unsuccessful. Please try again.'
+				})
 				// TODO: add error message here
 			})
 			.finally(() => {
@@ -54,26 +64,25 @@ export default function Register() {
 			<Helmet>
 				<title>Sign Up - TasteBuddies</title>
 			</Helmet>
-			<h1>Register</h1>
-			<p>Registration page works</p>
 			<form
-				className="[&>label>input]:rounded-md [&>label>input]:border-2 [&>label>input]:border-gray-400"
+				className="mx-auto flex flex-col sm:w-96 [&>input]:rounded-md [&>input]:border-2 [&>input]:border-gray-400 [&>label]:mt-2 [&>p]:text-red-700"
 				onSubmit={handleSubmit(onSubmit)}
 			>
+				<h2 className="text-4xl">Register</h2>
 				<label htmlFor="email" content="Email">
 					Email
-					<input
-						type="email"
-						id="email"
-						{...register('email', {
-							required: 'An email address is required',
-							pattern: {
-								value: /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
-								message: 'Must be an email',
-							},
-						})}
-					/>
 				</label>
+				<input
+					type="email"
+					id="email"
+					{...register('email', {
+						required: 'An email address is required',
+						pattern: {
+							value: /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+							message: 'Must be an email',
+						},
+					})}
+				/>
 				{errors.email?.type === 'required' && (
 					<p role="alert">{errors.email.message}</p>
 				)}
@@ -82,52 +91,48 @@ export default function Register() {
 				)}
 				<label htmlFor="displayName" content="displayName">
 					Display Name
-					<input
-						type="text"
-						id="display-name"
-						{...register('displayName', {
-							required: 'A display name is required!',
-							minLength: {
-								value: 3,
-								message: 'Display name must be at least 3 characters.',
-							},
-						})}
-					/>
 				</label>
+				<input
+					type="text"
+					id="display-name"
+					{...register('displayName', {
+						required: 'A display name is required!',
+						minLength: {
+							value: 3,
+							message: 'Display name must be at least 3 characters.',
+						},
+					})}
+				/>
 				{errors.displayName?.type === 'required' && (
 					<p role="alert">{errors.displayName.message}</p>
 				)}
 				{errors.displayName?.type === 'minLength' && (
 					<p role="alert">{errors.displayName.message}</p>
 				)}
-				<label htmlFor="password">
-					Password
-					<input
-						type="password"
-						id="password"
-						{...register('password', {
-							required: 'A password is required',
-							minLength: {
-								value: 7,
-								message: 'Password must be at least 7 characters',
-							},
-						})}
-					/>
-				</label>
-				<label htmlFor="confirmPassword">
-					Password
-					<input
-						type="password"
-						id="confirmPassword"
-						{...register('confirmPassword', {
-							required: 'A password is required',
-							minLength: {
-								value: 7,
-								message: 'Password must be at least 7 characters',
-							},
-						})}
-					/>
-				</label>
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					id="password"
+					{...register('password', {
+						required: 'A password is required',
+						minLength: {
+							value: 7,
+							message: 'Password must be at least 7 characters',
+						},
+					})}
+				/>
+				<label htmlFor="confirmPassword">Password</label>
+				<input
+					type="password"
+					id="confirmPassword"
+					{...register('confirmPassword', {
+						required: 'A password is required',
+						minLength: {
+							value: 7,
+							message: 'Password must be at least 7 characters',
+						},
+					})}
+				/>
 				{errors.password?.type && <p role="alert">{errors.password.message}</p>}
 				{(dirtyFields.password || touchedFields.password) &&
 					(dirtyFields.confirmPassword || touchedFields.confirmPassword) &&
@@ -135,13 +140,14 @@ export default function Register() {
 						<p role="alert">Passwords must match</p>
 					)}
 				<button
-					className="rounded-md bg-yellow-400 px-3 py-1 shadow-sm"
+					className="w-20 mt-4 rounded-md bg-yellow-400 px-3 py-1 shadow-sm"
 					type="submit"
 					id="submit"
 				>
 					Submit
 				</button>
 				{errorRes && <p role="alert">{errorRes}</p>}
+				{submitted && submissionError && <p role="alert">{submissionError}</p>}
 			</form>
 		</>
 	)
