7c335615925ad809380a7fef6ddb4619308479aa
Nathan Wright <natiouswright@gmail.com>
2023-11-06 20:54:41 -0600

    Added event page using state

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,13 +1,60 @@
+import { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
+import { Event } from '../models/Event'
+import EventService from '../services/EventService'
 
 export default function Events() {
+	const [upcommingEvents, setUpcomingEvents] = useState<Event[]>([])
+	const [pastEvents, setPastEvents] = useState<Event[]>([])
+
+	useEffect(() => {
+		const controller = new AbortController()
+
+		EventService.getEvents(controller)
+			.then((res) => {
+				for (const event of res.data) {
+					if (Number.parseInt(event.mealTime) > new Date().getTime()) {
+						setUpcomingEvents((currentEvents) => {
+							return [event].concat(...currentEvents)
+						})
+					} else {
+						setPastEvents((currentEvents) => {
+							return [event].concat(...currentEvents)
+						})
+					}
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+			.finally(() => {
+				console.log('Request to the server ran')
+			})
+		return () => {
+			controller.abort('Cancelled in cleanup')
+		}
+	}, [])
+
 	return (
 		<>
 			<Helmet>
 				<title>Events - TasteBuddies</title>
 			</Helmet>
 			<h2>Events</h2>
-			<p>Events works</p>
+			<p>Upcomming Events</p>
+			<ul>
+				{upcommingEvents.length === 0 && <p>No upcomming events. Go make one!</p>}
+				{upcommingEvents.map((event) => {
+					return <li key={event.entryCode}>{event.entryCode}</li>
+				})}
+			</ul>
+			<p>Past Events</p>
+			<ul>
+				{pastEvents.length === 0 && <p>No upcomming events. Go make one!</p>}
+				{pastEvents.map((event) => {
+					return <li key={event.entryCode}>{event.entryCode}</li>
+				})}
+			</ul>
 		</>
 	)
 }
